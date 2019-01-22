24ecddd442b5a6b8daa565a5f0047c5ed2a14b9e
Tài <iletai@hotmail.com>
2019-01-22 23:16:59 +0700

    ++ New Scene

---
+++ SpaceShooter/Assets/LevelLoader.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class LevelLoader : MonoBehaviour
+{
+    public void LoadLevel(int sceneIndex)
+    {
+        //AsyncOperation operation = SceneManager LoadSceneManager(sceneIndex);
+
+        StartCoroutine(LoadAsynchronously(sceneIndex));
+    }
+
+    IEnumerator LoadAsynchronously(int sceneIndex)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+
+        while (!operation.isDone)
+        {
+            Debug.Log(operation.progress);
+
+            yield return null;
+        }
+    }
+}
---
+++ SpaceShooter/Assets/LoadingBar.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadingBar : MonoBehaviour
+{
+    public Texture2D emptyProcessBar;
+    public Texture2D fullProcessBar;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
