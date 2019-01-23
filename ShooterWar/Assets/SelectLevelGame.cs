beaeec27613dfac40c8f7b5ca457e276836e26f2
iletai <iletai@hotmail.com>
2019-01-23 16:30:23 +0700

    +++ Select Level Scene

---
+++ ShooterWar/Assets/SelectLevelGame.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.UIElements;
+using UnityEngine.SceneManagement;
+
+public class SelectLevelGame : MonoBehaviour
+{
+
+    public GameObject loadingScene;
+     
+    public void LoadLevel(int sceneIndex)
+    {
+        StartCoroutine(LoadAsynchronously(sceneIndex));
+    }
+
+    IEnumerator LoadAsynchronously(int sceneIndex)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+        loadingScene.SetActive(true);
+        while (!operation.isDone)
+        {
+            
+            yield return null;
+        }
+    }
+}
