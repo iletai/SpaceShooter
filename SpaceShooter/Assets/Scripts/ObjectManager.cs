b86d6df9eb7ea0d700a6b8a9a77d9bf036c6c998
iletai <iletai@hotmail.com>
2019-01-23 11:48:36 +0700

    ++ ObjectManager

--- SpaceShooter/Assets/Scripts/ObjectManager.cs
+++ SpaceShooter/Assets/Scripts/ObjectManager.cs
@@ -2,21 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class ObjectManaer : MonoBehaviour
 {
-    public GameObject player;
-    public Transform target;
-    public float speed = 2.0f;
+
+    
 
     // Start is called before the first frame update
     void Start()
     {
-        speed = 10.0f;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        speed += 1;
+        
     }
 }
