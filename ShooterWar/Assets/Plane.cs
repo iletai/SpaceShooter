a98688c47456f05653ec08ff022749939629f99f
Tài <iletai@hotmail.com>
2019-01-25 23:08:08 +0700

    !! Implement move plane in scene

---
+++ ShooterWar/Assets/Plane.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plane : ObjectManager
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxis("Horizontal") < 0) //Move left
+        {
+            transform.position = Vector3.Lerp(transform.position, transform.position + Vector3.left, speedMove * Time.deltaTime);
+        }
+        else if (Input.GetAxis("Horizontal") > 0) //Move Right
+        {
+            transform.position = Vector3.Lerp(transform.position, transform.position - Vector3.left, speedMove * Time.deltaTime);
+        }
+        if (Input.GetAxis("Vertical") > 0) //Move up
+        {
+            transform.position = Vector3.Lerp(transform.position, transform.position + Vector3.forward, speedMove * Time.deltaTime);
+
+        }
+        else if (Input.GetAxis("Vertical") < 0) //Move down
+        {
+            transform.position = Vector3.Lerp(transform.position, transform.position - Vector3.forward, speedMove * Time.deltaTime);
+        }
+    }
+}
