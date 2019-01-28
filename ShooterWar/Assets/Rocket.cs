1e2fbb68d4acc4b5383709e4a55552d16eacebf7
iletai <iletai@hotmail.com>
2019-01-28 10:43:58 +0700

    ++ Add diagram for repo and !!Edit Rocket follow direction

--- ShooterWar/Assets/Rocket.cs
+++ ShooterWar/Assets/Rocket.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
+    public int dameRocket;
+    
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector3.Lerp(transform.position, transform.position += Vector3.forward, 8f * Time.deltaTime);
+        transform.position = Vector3.Lerp(transform.position, transform.position += Vector3.up, 8f * Time.deltaTime);
     }
 
     void CheckCollisionAnDestroy()
